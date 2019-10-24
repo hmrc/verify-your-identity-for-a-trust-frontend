5f8587dfe6081a7e6bfb62215b3343a8fe80fe0e
Adam Conder <adamdconder@gmail.com>
2019-10-24 11:15:27 +0100

    Add test route to mock IV success and failure

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -21,9 +21,10 @@ import controllers.routes
 import play.api.Configuration
 import play.api.i18n.Lang
 import play.api.mvc.Call
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 @Singleton
-class FrontendAppConfig @Inject() (configuration: Configuration) {
+class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig: ServicesConfig) {
 
   private val contactHost = configuration.get[String]("contact-frontend.host")
   private val contactFormServiceIdentifier = "claim-a-trust-frontend"
@@ -56,14 +57,17 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   lazy val relationshipIdentifier : String =
     configuration.get[String]("microservice.services.self.relationship-establishment.identifier")
 
-  private def relationshipEstablishmentPath(utr: String) : String =
+  private def relationshipEstablishmentFrontendPath(utr: String) : String =
     s"${configuration.get[String]("microservice.services.relationship-establishment-frontend.path")}/$utr"
 
-  private def relationshipEstablishmentHost : String =
+  private def relationshipEstablishmentFrontendHost : String =
     configuration.get[String]("microservice.services.relationship-establishment-frontend.host")
 
-  def relationshipEstablishmentUrl(utr: String) : String =
-    s"${relationshipEstablishmentHost}/${relationshipEstablishmentPath(utr)}"
+  def relationshipEstablishmenFrontendtUrl(utr: String) : String =
+    s"${relationshipEstablishmentFrontendHost}/${relationshipEstablishmentFrontendPath(utr)}"
+
+
+  def relationshipEstablishmentBaseUrl : String = servicesConfig.baseUrl("test.relationship-establishment")
 
   def languageMap: Map[String, Lang] = Map(
     "english" -> Lang("en"),
