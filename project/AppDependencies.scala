0b2a4c970b5cceceda869aa629e9f07b50656475
Adam Conder <adamdconder@gmail.com>
2020-09-04 15:33:20 +0100

    TRUS-2851 Upgrade sbt and scaffold plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,14 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.3-play26",
-    "uk.gov.hmrc"       %% "logback-json-logger"            % "3.1.0",
-    "uk.gov.hmrc"       %% "govuk-template"                 % "5.36.0-play-26",
+    "org.reactivemongo" %% "play2-reactivemongo"            % "0.18.6-play26",
+    "uk.gov.hmrc"       %% "logback-json-logger"            % "4.6.0",
+    "uk.gov.hmrc"       %% "govuk-template"                 % "5.52.0-play-26",
     "uk.gov.hmrc"       %% "play-health"                    % "3.14.0-play-26",
-    "uk.gov.hmrc"       %% "play-ui"                        % "8.2.0-play-26",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "0.2.0",
-    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.1.0",
-    "uk.gov.hmrc"       %% "play-whitelist-filter"          % "2.0.0"
+    "uk.gov.hmrc"       %% "play-ui"                        % "8.8.0-play-26",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.2.0-play-26",
+    "uk.gov.hmrc"       %% "bootstrap-play-26"              % "1.4.0",
+    "uk.gov.hmrc"       %% "play-whitelist-filter"          % "3.1.0-play-26"
   )
 
   val test = Seq(
@@ -25,6 +25,17 @@ object AppDependencies {
     "org.scalacheck"              %% "scalacheck"         % "1.14.0",
     "com.github.tomakehurst"      % "wiremock-standalone"      % "2.17.0"
   ).map(_ % Test)
+
+  val akkaVersion = "2.5.23"
+  val akkaHttpVersion = "10.0.15"
+
+  val overrides = Seq(
+    "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+    "com.typesafe.akka" %% "akka-protobuf" % akkaVersion,
+    "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
+    "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+    "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion
+  )
 
   def apply(): Seq[ModuleID] = compile ++ test
 }
