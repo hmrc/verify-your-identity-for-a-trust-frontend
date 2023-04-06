8753f258c8d4c59ac07317506626e3c5ce49b2e1
Gaston Racui <gastonracui@gmail.com>
2023-04-06 14:33:28 +0100

    DDCE-3931 Upgrade to build with jdk 11 and scala 2.13

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val bootstrapVersion = "7.14.0"
+  val bootstrapVersion = "7.15.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
@@ -19,8 +19,8 @@ object AppDependencies {
     "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "org.scalatestplus.play"      %% "scalatestplus-play"       % "5.1.0",
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % "0.74.0",
-    "com.vladsch.flexmark"        % "flexmark-all"              % "0.62.0",
-    "org.jsoup"                   %  "jsoup"                    % "1.15.3",
+    "com.vladsch.flexmark"        % "flexmark-all"              % "0.64.0",
+    "org.jsoup"                   %  "jsoup"                    % "1.15.4",
     "com.typesafe.play"           %% "play-test"                % PlayVersion.current,
     "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.12",
     "com.github.tomakehurst"      % "wiremock-standalone"       % "2.27.2"
