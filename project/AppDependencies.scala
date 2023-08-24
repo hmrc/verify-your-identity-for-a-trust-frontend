a2578b0fa0bd9a2b12bc22129592a10b903fd45b
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2023-08-24 15:30:58 +0100

    DDCE-4530: Changes for sm2 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.17.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion
   )
