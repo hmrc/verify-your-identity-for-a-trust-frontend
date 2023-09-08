0664ab4a7b89a624f329e37cfc22ecb56bfb14d5
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2023-09-08 10:36:33 +0100

    DDCE-4530: Some additional dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.21.0"
+  val bootstrapVersion = "7.22.0"
   val mongoVersion = "1.3.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -16,12 +16,12 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % mongoVersion,
-    "org.scalatest"               %% "scalatest"                % "3.2.16",
-    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
-    "com.vladsch.flexmark"        % "flexmark-all"              % "0.64.8",
+    "org.scalatest"               %% "scalatest"                % "3.2.17",
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.17.0",
+    "com.vladsch.flexmark"        %  "flexmark-all"              % "0.64.8",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
-    "com.github.tomakehurst"      % "wiremock-standalone"       % "2.27.2"
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
+    "org.wiremock"                % "wiremock-standalone"       % "3.0.3"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
