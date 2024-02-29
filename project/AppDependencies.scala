ba265fc95c25096949e123301a384d4da35e1ca8
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2024-02-29 13:14:52 +0000

    DDCE-5380: Update HMRC logo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt.*
 object AppDependencies {
 
   val bootstrapVersion = "7.22.0"
-  val mongoVersion = "1.3.0"
+  val mongoVersion = "1.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-28"     % "8.5.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion
   )
@@ -16,12 +16,12 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % mongoVersion,
-    "org.scalatest"               %% "scalatest"                % "3.2.17",
-    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.17.0",
-    "com.vladsch.flexmark"        %  "flexmark-all"              % "0.64.8",
-    "org.jsoup"                   %  "jsoup"                    % "1.16.1",
+    "org.scalatest"               %% "scalatest"                % "3.2.18",
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
+    "com.vladsch.flexmark"         %  "flexmark-all"              % "0.64.8",
+    "org.jsoup"                   %  "jsoup"                    % "1.17.2",
     "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
-    "org.wiremock"                % "wiremock-standalone"       % "3.0.3"
+    "org.wiremock"                % "wiremock-standalone"       % "3.4.2",
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
