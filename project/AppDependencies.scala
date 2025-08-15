59363df12f19a9c5ce33af6ee8322a29842647f5
maheshhvr <175945803+maheshhvr@users.noreply.github.com>
2025-08-15 13:45:42 +0100

    DDCE-6964 [Trusts]: Fix Jackson Core Vulnerability [verify-your-identity-for-a-trust-frontend] scalacheck dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,7 +16,7 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-30"  % mongoVersion,
-    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
+    "org.scalatestplus"           %% "scalacheck-1-18"          % "3.2.19.0",
     "org.jsoup"                   %  "jsoup"                    % "1.21.1"
   ).map(_ % Test)
 
